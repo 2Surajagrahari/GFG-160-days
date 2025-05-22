c0ce2ec1511b8148b0fb531266f11f7135a94f60
Suraj Agrahari <surajagarahari265@gmail.com>
2025-05-22 16:30:16 +0530

    Second largest

---
+++ codes/Second Largest.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int getSecondLargest(int[] arr) {
+        // code here
+        int largest = Integer.MIN_VALUE;
+        int secondLargest = Integer.MIN_VALUE;
+        for(int i=0;i<arr.length;i++){
+            if(largest<arr[i]){
+                secondLargest = largest;
+                largest = arr[i];
+            }
+            else if(arr[i] > secondLargest && arr[i] < largest){
+                secondLargest = arr[i];
+            }git
+        }
+        
+        if(secondLargest == Integer.MIN_VALUE){
+            return -1;
+        }
+        else{
+            return secondLargest;
+        }
+    }
+}
