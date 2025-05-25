1e89486fc50e4b0cd4612a930ba53bbde3e0ba4d
Suraj Agrahari <surajagarahari265@gmail.com>
2025-05-25 19:59:09 +0530

    Majority Element II

---
+++ codes/Majority Element II.java
@@ -0,0 +1,55 @@
+class Solution {
+    // Function to find the majority elements in the array
+    public List<Integer> findMajority(int[] nums) {
+        // Your code goes here.
+        List<Integer> result = new ArrayList<>();
+        if(nums == null || nums.length == 0){
+            return result;
+        }
+        int candidate1 = 0, candidate2 = 0;
+        int count1 = 0, count2 = 0;
+        
+        for(int num:nums){
+            if(num == candidate1){
+                count1++;
+            }
+            else if(num == candidate2){
+                count2++;
+            }
+            else if(count1 == 0){
+                candidate1 = num;
+                count1 = 1;
+            }
+            else if(count2 == 0){
+                candidate2 = num;
+                count2 = 1;
+            }
+            else{
+                count1--;
+                count2--;
+            }
+        }
+        
+        count1 = 0;
+        count2 = 0;
+        for(int num: nums){
+            if(num == candidate1){
+                count1++;
+            }
+            else if(num == candidate2){
+                count2++;
+            }
+        }
+        int n = nums.length;
+        if(count1>n/3){
+            result.add(candidate1);
+        }
+        if(count2>n/3){
+            result.add(candidate2);
+        }
+        
+        
+        Collections.sort(result);
+        return result;
+    }
+}
